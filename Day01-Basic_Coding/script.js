c68d3579848523e800d166fddfa3583d444a55bb
satya kumari <satyakumarigrd2004@gmail.com>
2025-08-09 22:29:12 +0530

    practice basic code

---
+++ Day01-Basic_Coding/script.js
@@ -0,0 +1,36 @@
+// How to print output
+console.log("Hello World");
+
+age=24;
+// console.log("My age is",age);
+// console.log("My age is "+age);
+// console.log(`My age is ${age}`);
+
+
+//          NOTE :- Javascript is a dynamically typed language.
+//                  Javascript compile code line by line.
+//                  we cannot use keywords as a variable name.
+
+a = 10;
+// console.log(a);
+a = "satya";
+// console.log(a);
+b = 5;
+// console.log(b);
+b = b++;
+// console.log(b);
+b = ++b;
+// console.log(b);
+
+var a=5;
+console.log(a);   //output -- 5
+var a=6;
+console.log(a);   //output --6
+
+const a=5;
+console.log(a);
+var a=9;                    //ERROR
+console.log(a);
+
+
+

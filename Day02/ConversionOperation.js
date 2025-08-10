681924c6add4ddcd395ee02776870bb4dc0e6504
satya kumari <satyakumarigrd2004@gmail.com>
2025-08-10 23:40:30 +0530

    conversion Operation

---
+++ Day02/ConversionOperation.js
@@ -0,0 +1,15 @@
+let value=3;
+let negValue=-value;
+console.log(negValue);
+
+let str1 = "hello";
+let str2 = " hitesh";
+let str3=str1+str2;
+console.log(str3);          //hello hitesh
+
+
+console.log("1"+2);             //12
+console.log(1+"2");             //12
+console.log(1+1+"2");           //22
+console.log("1"+1+2);           //112
+console.log(1+1+"2"+1+1);         //2211

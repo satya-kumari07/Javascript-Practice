cb8aba0f0ef6fc0294f669caab13792ef2ab5ad9
satya kumari <satyakumarigrd2004@gmail.com>
2025-08-11 19:07:54 +0530

    fix some bugs

--- Day01-Basic_Coding/operator.js
+++ Day01-Basic_Coding/operator.js
@@ -36,3 +36,31 @@ console.log(d=10?"yes":"no");
 console.log(d>e && a>b);
 console.log(d>e || d==0);
 console.log(!(d==11));
+
+
+// *********************UNARY OPERATORS**********************
+let age = 10;
+let newAge = ++age;
+console.log(newAge);     //11
+
+let age2=10;
+var newAge2 = age2++;
+console.log(newAge2);    //10
+
+let age3=10;
+age3++;
+console.log(age3);              //11
+
+let age4=10;
+++age4;
+console.log(age4);              //11
+
+let age1=10;
+console.log(age1++);        //10
+
+let age5=10;
+console.log(++age5);            //11
+
+let num=5;              //num=5
+let newNum=num++;       // newNum=5  num=6
+newNum=++num;           // newNum=7

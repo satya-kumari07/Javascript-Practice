e2265875ee3611f73a793d7f641d7b87baa0c8c7
satya kumari <satyakumarigrd2004@gmail.com>
2025-08-11 19:21:09 +0530

    learn rules of identifiers

---
+++ Day02/identifiers.js
@@ -0,0 +1,25 @@
+let age;
+
+let _age;
+
+let $age;
+
+// let 1age;       //error
+
+// let new name;      //error
+
+let num1;
+
+let new_name;
+
+let new$name;
+
+//names are case sensitive y and Y are different
+//reserved words cannot be used as varaiable name
+
+
+//++++++++++++++  WAY OF WRITING IDENTIFIERS  ++++++++++++++++
+
+//        =>  camelCase
+//        =>  snake_case
+//        =>  PascalCase
